fc9f83a3c4a64bfb9ffabbde78997f993740763a
Udeme98 <Darealudeme1@gmail.com>
2024-06-17 15:12:36 +0100

    NPM

---
+++ NPM/index.js
@@ -0,0 +1,12 @@
+import superheroes from "superheroes";
+// var generateName = require("sillyname");
+
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(sillyName);
+
+// const superheroes = require("superheroes");
+
+var superheroName = superheroes.random();
+console.log(superheroName);
