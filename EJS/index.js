9e54c3a007f24a863c454d51fc559268ee6e2390
Udeme98 <Darealudeme1@gmail.com>
2024-06-20 09:27:39 +0100

    EJS

--- EJS/index.js
+++ EJS/index.js
@@ -2,6 +2,24 @@ import express from "express";
 const app = express();
 const port = 3001;
 
+app.get("/", (req, res) => {
+  const today = new Date();
+  const day = today.getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
+  }
+
+  res.render("index.ejs", {
+    dayType: type,
+    advice: adv,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
