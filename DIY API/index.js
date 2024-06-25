0f9711ae45a4a8815495337ee8442f86c4834b46
Udeme98 <Darealudeme1@gmail.com>
2024-06-25 23:20:42 +0100

    DIY API: PUT and PATCH

--- DIY API/index.js
+++ DIY API/index.js
@@ -48,8 +48,32 @@ app.post("/jokes", (req, res) => {
 });
 
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const { jokeText, jokeType } = req.body;
+
+  const jokeIndex = jokes.findIndex((j) => j.id === id);
+  jokes[jokeIndex] = { id, jokeText, jokeType };
+
+  res.json(jokes[jokeIndex]);
+});
 
 //6. PATCH a joke
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+});
 
 //7. DELETE Specific joke
 
