9a9fb658431328b71690b74c4a28a22e485fc998
Udeme98 <Darealudeme1@gmail.com>
2024-07-05 22:50:33 +0100

    Databases: World Capital Quiz

--- World Capital Quiz/index.js
+++ World Capital Quiz/index.js
@@ -1,14 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-let quiz = [
-  { country: "France", capital: "Paris" },
-  { country: "United Kingdom", capital: "London" },
-  { country: "United States of America", capital: "New York" },
-];
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Udeme1998",
+  port: 5432,
+});
+
+db.connect();
+
+let quiz = [];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
 
 let totalCorrect = 0;
 
